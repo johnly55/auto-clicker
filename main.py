59d5eea35471e1c291d238570fe58cb547aa19f6
John Ly <john.ly55@outlook.com>
2023-09-27 19:07:39 -0600

    Fixed main.py to use gui.py GUI class.

--- main.py
+++ main.py
@@ -1,23 +1,15 @@
 import tkinter as tk
 import pyautogui
-from pynput.mouse import Button, Controller 
+from pynput.mouse import Button, Controller
+
+from gui import GUI
 
 APP_NAME = "John Ly's Auto Clicker"
 
 
 def main():
-    root = tk.Tk()
-    root.title("John Ly's Auto Clicker")
-
-    menu_bar_fr = tk.Frame(root)
-    actions_fr = tk.Frame(root)
-    design_fr = tk.Frame(root)
-
-    menu_bar_fr.grid(row=0, column=0)
-    actions_fr.grid(row=1, column=0)
-    design_fr.grid(row=1, column=1)
-
-    root.mainloop()
+    gui = GUI(APP_NAME)
+    gui.root.mainloop()
 
 if __name__ == '__main__':
     main()
